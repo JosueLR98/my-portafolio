c9637d805abbba7199a02d94ecf2002400e2508a
Josue Lopez <josue12131415@gmail.com>
2023-06-12 20:52:03 -0600

    porfolio-intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 function Intro() {
   return (
-    <div id="intro" className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+    <div
+      id="intro"
+      className="flex items-center justify-center flex-col text-center pt-20 pb-6"
+    >
       <div></div>
       <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold underline hover:underline-offset-8   ">
         Josue Lopez
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">Web Developer</p>
       <p className="text-lg max-w-xl mb-3 font-bold">
-        Soy un desarrollador full-stack condominio de html, css, javascript,
+        Soy un desarrollador full-stack con dominio de html, css, javascript,
         react y redux inicie mi carrera con gran motivacion y actualmete
         estudiante de ingeniería informática, mis proyectos son desde cero,
         desde la planificación y el diseño hasta el final agradezco su tiempo
